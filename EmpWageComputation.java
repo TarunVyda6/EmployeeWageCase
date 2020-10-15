0b173d578b4ed77615a5da31fadb4e45d2f80e89
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-10-15 21:32:36 +0530

    monthly wages calculation feature is added

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -6,25 +6,31 @@ public class EmpWageComputation
 		public static final int IS_FULL_TIME = 1;
 		public static final int IS_PART_TIME = 2;
 		public static final int WAGE_PER_HOUR = 20;
+		public static final int NUMBER_OF_WORKING_DAYS = 20;
 	
 	public static void main(String[] args) 
 	{
 		Random random = new Random();
 		// Variables
-			int empHours = 0;
-			int dailyEmployeeWage = 0;
+			int totalEmployeeWage = 0;
 		// Computation
-			int empStatus = random.nextInt(3);
-			
-			switch( empStatus )
+			for ( int day = 1; day <= NUMBER_OF_WORKING_DAYS; day++ )
 			{
-			case IS_FULL_TIME	: empHours = 8;
-				 break;
-			case IS_PART_TIME	: empHours = 4;
-				 break;
-			default			: empHours = 0;
+				int empHours = 0;
+				int dailyEmployeeWage = 0;
+				int empStatus = random.nextInt(3);
+				switch( empStatus )
+				{
+				case IS_FULL_TIME	: empHours = 8;
+					 break;
+				case IS_PART_TIME	: empHours = 4;
+					 break;
+				default			: empHours = 0;
+				}
+				dailyEmployeeWage = empHours * WAGE_PER_HOUR;
+				System.out.println( "Daily Employee Wage is : " + dailyEmployeeWage );
+				totalEmployeeWage = totalEmployeeWage + dailyEmployeeWage;
 			}
-			dailyEmployeeWage = empHours * WAGE_PER_HOUR;
-			System.out.println("Daily Employee Wage is : " + dailyEmployeeWage );
+			System.out.println( "Total Employee wage is : " + totalEmployeeWage );
 	}
 }
