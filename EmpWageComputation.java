e553ba905551159eb36e89953521d12a7e0cd793
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-10-17 00:33:30 +0530

    added feature of employee wage and total wage by company

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,44 +5,62 @@ public class EmpWageComputation
 	// Constants
 		public static final int IS_FULL_TIME = 1;
 		public static final int IS_PART_TIME = 2;
-	public static void main(String[] args) 
-	{
-		System.out.print("emp wage for abc company is ");
-		WageCalculation(20,20,100);
-		System.out.print("emp wage for xyz company is ");
-		WageCalculation(45,20,100);
+		private final int wagePerHour;
+		private final int numberOfWorkingDays;
+		private final int numberOfWorkingHours;
+		private final String company;
+		private int totalEmployeeWage;
+		
+		public static void main(String[] args) 
+		{
+			EmpWageComputation microSoft = new EmpWageComputation ( "MICROSOFT", 20, 20, 100 );
+			microSoft.wageCalculation();
+			EmpWageComputation google = new EmpWageComputation ( "GOOGLE", 45, 20, 100 );
+			google.wageCalculation ();
+			System.out.println(microSoft);
+			System.out.println( google );
+		}
+		
 
-	}
-	
-	public static void wageCalculation ( int wagePerHour, int numberOfWorkingDays, int numberOfWorkingHours )
-	{
-		Random random = new Random ();
-		// Variables
-			int totalEmployeeWage = 0;
-			int day = 0;
-			int workingHours = 0;
-		// Computation
-			while ( day < numberOfWorkingDays && workingHours < numberOfWorkingHours )
-			{
-				int empHours = 0;
-				int dailyEmployeeWage = 0;
-				int empStatus = random.nextInt (3);
-				switch( empStatus )
+		public EmpWageComputation( String company, int wagePerHour, int numberOfWorkingDays, int numberOfWorkingHours)
+	    {
+	            this.company=company;
+	            this.wagePerHour=wagePerHour;
+	            this.numberOfWorkingDays=numberOfWorkingDays;
+	            this.numberOfWorkingHours=numberOfWorkingHours;
+	    }
+		
+		public void wageCalculation ()
+		{
+			Random random = new Random ();
+			// Variables
+				int day = 0;
+				int workingHours = 0;
+			// Computation
+				while ( day < numberOfWorkingDays && workingHours < numberOfWorkingHours )
 				{
-				case IS_FULL_TIME	: empHours = 8;
-							break;
-				case IS_PART_TIME	: empHours = 4;
-							break;
-				default			: empHours = 0;
+					int empHours = 0;
+					int dailyEmployeeWage = 0;
+					int empStatus = random.nextInt (3);
+					switch( empStatus )
+					{
+					case IS_FULL_TIME	: empHours = 8;
+								break;
+					case IS_PART_TIME	: empHours = 4;
+								break;
+					default			: empHours = 0;
+					}
+					workingHours =  workingHours + empHours;
+					day++;
+					dailyEmployeeWage = empHours * wagePerHour;
+					System.out.println( "Daily Employee Wage for " + company + "is : " + dailyEmployeeWage );
+					totalEmployeeWage = totalEmployeeWage + dailyEmployeeWage;
 				}
-				workingHours =  workingHours + empHours;
-				day++;
-				dailyEmployeeWage = empHours * wagePerHour;
-				System.out.println( "Daily Employee Wage is : " + dailyEmployeeWage );
-				totalEmployeeWage = totalEmployeeWage + dailyEmployeeWage;
-			}
-			
-			System.out.println( "Total Employee wage is : " + totalEmployeeWage );
-	}
+				
+		}
+		public String toString()
+		{
+			return "total employee wage for " + company + " is : " + totalEmployeeWage;
+		}
 	
 }
