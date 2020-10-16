dc08c2d336314fd5d93e94b84da0e5099d91681a
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-10-16 23:23:04 +0530

    added feature of writing entire code in class method

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,17 +1,24 @@
+package org.empwagecomputation;
+
 import java.util.Random;
 
 public class EmpWageComputation 
 {
 	// Constants
-		public static final int IS_FULL_TIME = 1;
-		public static final int IS_PART_TIME = 2;
-		public static final int WAGE_PER_HOUR = 20;
-		public static final int NUMBER_OF_WORKING_DAYS = 20;
-		public static final int NUMBER_OF_WORKING_HOURS = 100;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int WAGE_PER_HOUR = 20;
+	public static final int NUMBER_OF_WORKING_DAYS = 20;
+	public static final int NUMBER_OF_WORKING_HOURS = 100;
 	
 	public static void main(String[] args) 
 	{
-		Random random = new Random();
+		wageCalculation ();
+	}
+	
+	public static void wageCalculation ()
+	{
+		Random random = new Random ();
 		// Variables
 			int totalEmployeeWage = 0;
 			int day = 0;
@@ -21,13 +28,13 @@ public class EmpWageComputation
 			{
 				int empHours = 0;
 				int dailyEmployeeWage = 0;
-				int empStatus = random.nextInt(3);
+				int empStatus = random.nextInt (3);
 				switch( empStatus )
 				{
 				case IS_FULL_TIME	: empHours = 8;
-					 break;
+							break;
 				case IS_PART_TIME	: empHours = 4;
-					 break;
+							break;
 				default			: empHours = 0;
 				}
 				workingHours =  workingHours + empHours;
@@ -39,4 +46,5 @@ public class EmpWageComputation
 			
 			System.out.println( "Total Employee wage is : " + totalEmployeeWage );
 	}
+	
 }
