4c941c31b0e0aaa3b1f55daf7b89a8ae9c4cb223
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-10-16 23:41:22 +0530

    added feature of passing values for multiple companies in procedural way

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,22 +1,20 @@
-package org.empwagecomputation;
-
 import java.util.Random;
 
 public class EmpWageComputation 
 {
 	// Constants
-	public static final int IS_FULL_TIME = 1;
-	public static final int IS_PART_TIME = 2;
-	public static final int WAGE_PER_HOUR = 20;
-	public static final int NUMBER_OF_WORKING_DAYS = 20;
-	public static final int NUMBER_OF_WORKING_HOURS = 100;
-	
+		public static final int IS_FULL_TIME = 1;
+		public static final int IS_PART_TIME = 2;
 	public static void main(String[] args) 
 	{
-		wageCalculation ();
+		System.out.print("emp wage for abc company is ");
+		WageCalculation(20,20,100);
+		System.out.print("emp wage for xyz company is ");
+		WageCalculation(45,20,100);
+
 	}
 	
-	public static void wageCalculation ()
+	public static void wageCalculation ( int wagePerHour, int numberOfWorkingDays, int numberOfWorkingHours )
 	{
 		Random random = new Random ();
 		// Variables
@@ -24,7 +22,7 @@ public class EmpWageComputation
 			int day = 0;
 			int workingHours = 0;
 		// Computation
-			while ( day < NUMBER_OF_WORKING_DAYS && workingHours < NUMBER_OF_WORKING_HOURS)
+			while ( day < numberOfWorkingDays && workingHours < numberOfWorkingHours )
 			{
 				int empHours = 0;
 				int dailyEmployeeWage = 0;
@@ -39,7 +37,7 @@ public class EmpWageComputation
 				}
 				workingHours =  workingHours + empHours;
 				day++;
-				dailyEmployeeWage = empHours * WAGE_PER_HOUR;
+				dailyEmployeeWage = empHours * wagePerHour;
 				System.out.println( "Daily Employee Wage is : " + dailyEmployeeWage );
 				totalEmployeeWage = totalEmployeeWage + dailyEmployeeWage;
 			}
